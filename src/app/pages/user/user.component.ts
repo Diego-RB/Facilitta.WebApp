f868fb88c39958ec62d93dcead48148e0bd9a79f
IATec - Eric Oliveira <eric.oliveira@iatec.com>
2023-12-07 11:23:03 -0300

    Mudanças

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { publicDecrypt } from "crypto";
+import * as e from "express";
 
 @Component({
-    selector: 'user-cmp',
-    moduleId: module.id,
-    templateUrl: 'user.component.html'
+  selector: "user-cmp",
+  moduleId: module.id,
+  templateUrl: "user.component.html",
 })
+export class UserComponent implements OnInit {
+  public isExpense: boolean = false;
+  public isDeposit: boolean = false;
 
-export class UserComponent implements OnInit{
-    ngOnInit(){
+  ngOnInit() {}
+
+  public changeCheckbox(event: any, type: string): void {
+    if (type == "expense") {
+      this.isExpense = event.target.checked;
+      if (this.isExpense) {
+        this.isDeposit = false; // Desmarca o checkbox de depósito
+      }
+    } else if (type == "deposit") {
+      this.isDeposit = event.target.checked;
+      if (this.isDeposit) {
+        this.isExpense = false; // Desmarca o checkbox de despesa
+      }
     }
+  }
 }
